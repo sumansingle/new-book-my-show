0cf8346ded8e5df7c58195fd72b049792bca4747
suman <ghoshs91439@gmail.com>
2023-03-20 13:41:30 +0530

    book-my-show work on checkout page

--- src/components/WishList/WishListCard.jsx
+++ src/components/WishList/WishListCard.jsx
@@ -1,20 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import noImage from "../utills/images/no-image.png";
 import { apiUrl } from "../important/api";
 import { FaStar } from "react-icons/fa";
 
 export function WishListCard(props) {
-    const createGenres = (arr) => {
-       return arr.map((item,index)=> {
-            return <span key={`span${index}`}>{item.name}</span>
-        })
-    }
+  const createGenres = (arr) => {
+    return arr.map((item, index) => {
+      return <span key={`span${index}`}>{item.name}</span>;
+    });
+  };
+
+  const handleCloseButtonClick = (id) => {
+    // Remove the item from local storage
+    let storedItems = JSON.parse(localStorage.getItem("wishlist")) || [];
+    storedItems = storedItems.filter((item) => item.id !== id);
+    localStorage.setItem("wishlist", JSON.stringify(storedItems));
+
+    // Update the state to hide the component
+    setIsClosed(true);
+  };
+
+  const [isClosed, setIsClosed] = useState(false);
+
+  if (isClosed) {
+    return null;
+  }
 
   return (
     <div className="wraper-card" key={`wraper${props.index}`}>
       <img
         src={
-         props.wishData.detils.poster_path === null
+          props.wishData.detils.poster_path === null
             ? noImage
             : apiUrl.imageBase + props.wishData.detils.poster_path
         }
@@ -22,12 +38,13 @@ export function WishListCard(props) {
         key={`img${props.index}`}
       />
       <div className="wrap-wishlist-data" key={`div${props.index}`}>
-      <h3 key={`title-wish${props.index}`}>{props.wishData.detils.title}</h3>
-      <p key={`ptag${props.index}`}>
-      <FaStar /> {props.wishData.detils.vote_average}/10
-      </p>
-      <div key={`genres${props.index}`}>{createGenres(props.wishData.data)}</div>
-      <p key={`overview${props.index}`}>{props.wishData.detils.overview}</p>
+        <h3 key={`title-wish${props.index}`}>{props.wishData.detils.title}</h3>
+        <p key={`ptag${props.index}`}>
+          <FaStar /> {props.wishData.detils.vote_average}/10
+        </p>
+        <div key={`genres${props.index}`}>{createGenres(props.wishData.data)}</div>
+        <p key={`overview${props.index}`}>{props.wishData.detils.overview}</p>
+        <button onClick={() => handleCloseButtonClick(props.wishData.detils.id)}>close</button>
       </div>
     </div>
   );
